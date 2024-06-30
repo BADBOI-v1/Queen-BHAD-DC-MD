2cafb612478ff16e299c0106a9523b30faf4f6a1
Reece-lab <ianreece648@gmail.com>
2024-07-01 02:36:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.sudo = process.env.SUDO
   : "null";
 global.owner = process.env.OWNER_NUMBER
   ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
-  : "2349066528353";
+  : "254783816038";
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
