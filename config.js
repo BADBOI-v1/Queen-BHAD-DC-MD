0a352cdc594b1c262a22fe2c2a4ab316ce63a247
TamseelRiaz <km8709848@gmail.com>
2024-07-05 21:39:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYUFxQkx4NDVpNCsyM1F2VzRpQkNNeTZXK1MyZzVHbHBPZFdNRitlMTIzRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN3dKODJTQWh3cEdBcjBKcGVFeDIwTDFXZlhLMmF4YTNpVTlsT2pLWWcwMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1UEJ0RTVkRFdhSlBoMXAreXZtVm0zWmo5cDEvN1lKNHowWEhySVNjRDA0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJOSWwzZHM0bk5XMFZpYXdwS2phbm0rTlcyVnk4dzdSV2w2RG1HejduVG5FPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFGY1d3TGZrdlpIZlIrWG5ZdzFZSFNoMEs0Y3hIbEtrSGswOU1iNHlLbUk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImF4eUEvdk80ZWsrVkFFK0hQNFVEelpkYXhzMTlJWmNQS2ZCYW5SODlWMzQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMk9Qa2ljdTREdWhuOTFiMHk1V0dTVEVmaytSWk5ZWG4xcUhvSTB5b3BXZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic2RlRXJOWk9KLytmSzVJSVY5S0NuMFVQNklCM05mNy9VdkFhOTNNdnBWST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFyUUdybGVLN1Y5UXJib0tEOGtjbElxWU41TkNRZk5DMmdzcEEydkFFa3M3dlBHanVYb3JBUVdPRUdOR3NLZGxsR3MzU3IrRDVlTisvcGRwY3h3d2d3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MzQsImFkdlNlY3JldEtleSI6IlM3dUtvTEc2OVFhS2pHTFdyME5pR0sreWlWeFNFZFNkUCsyb2VoQU9Fbm89IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbeyJrZXkiOnsicmVtb3RlSmlkIjoiOTIzMTQyMjEyMzgzQHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IkI2OTA3NkI3MUFGQjIwQUE3RUY0MTU4RDI3QjQxNzVBIn0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3MjAxOTcyMzN9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImEyRzh1N2ZoUS11SGlrUExjRW9yc1EiLCJwaG9uZUlkIjoiYThlODhkN2MtMWEyZC00MTQ0LWJhYzktOGQyNzQ1MmY1MWRkIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik42RVZjN1FkZm84WnR3bU9sWnprdEZuZndQUT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrSkJUUGV6WDVxdkhneDNoaXVnMFYrVHk4eVU9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiNENGSFMzVkUiLCJtZSI6eyJpZCI6IjkyMzE0MjIxMjM4MzozM0BzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLwnZmOIPCdnpwg8J2ekyDwnZ6gIPCdnqIifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ09uWjRvMENFT0hBb0xRR0dDUWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IjRYcGJ3K3lxLzNyR2kyOGxKSVNLTzhDc3ZxMktYYzFaSlU1MFZyTkpiVnc9IiwiYWNjb3VudFNpZ25hdHVyZSI6IitUTzdEazIyVHFqc2FZL2d0Nkk1UHdRVjJWUjJ3QllXYnhzK1VNanh1eWMxRDRTZy8xellpUENXeTJSN20rcTgvWW1MTjNiVm4vUnZ3NUwxQ3NjUkFRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJmNk9Ed0FjZmNTVDBLOS9FVTg0VWlOMkNWRWxxMFlaNi8zaysxbnZMMW5adndCbitWeHA1UnBFOXJBbnZKaFJIL0NyamVFbXErLzBabmI0MFBQUVBoQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzE0MjIxMjM4MzozM0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJlRjZXOFBzcXY5NnhvdHZKU1NFaWp2QXJMNnRpbDNOV1NWT2RGYXpTVzFjIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwMTk3MjMwLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUJyeSJ9";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
@@ -16,10 +16,10 @@ global.owner = process.env.OWNER_NUMBER
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
-  "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg,https://telegra.ph/file/7275967ae7b5283fada69.jpg";
+  "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg,https://telegra.ph/file/de395cdf0fbca8605f964.jpg";
 global.userImages =
   process.env.USER_IMAGES ||
-  "https://telegra.ph/file/7275967ae7b5283fada69.jpg,https://telegra.ph/file/c3049cd3ac77f371e119e.jpg,https://telegra.ph/file/a22200a780671e0e32383.jpg,https://telegra.ph/file/85fe388fdd14930cf86a0.jpg,https://telegra.ph/file/ba9ced500f9eca7db8acb.mp4";
+  "https://telegra.ph/file/71bf5ba1dd522a6ad8579.mp4";
 ///===========[global iMPORTS]====================//
 
 module.exports = {
@@ -27,11 +27,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`©QUEEN_ANITA-V2`",
-  author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
-  packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  caption: process.env.CAPTION || "`©SNAPE-V2`",
+  author: process.env.PACK_AUTHER || "SNAPE-V2",
+  packname: process.env.PACK_NAME || "S N A P E",
+  botname: process.env.BOT_NAME || "SNAPE-V2",
+  ownername: process.env.OWNER_NAME || "Tamseel Riaz",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -46,8 +46,8 @@ module.exports = {
   LANG: (process.env.THEME || "WhatsApp").toUpperCase(),
 };
 global.port = process.env.PORT;
-global.appUrl = process.env.APP_URL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
-global.email = "davidcyril209@gmail.com";
+global.appUrl = process.env.APP_URL || "https://chat.whatsapp.com/Kz2z5hSFpab6x3qsto3JkB";
+global.email = "akhundtamseel913@gmail.com ";
 global.location = "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
